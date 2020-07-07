fa9624920d14e9604daf18509d6d48b54b80ca9b
divyakhetan <divyakhetan1209@gmail.com>
2020-07-07 15:31:07 +0530

    added camera module for both screens

--- index.py
+++ index.py
@@ -4,26 +4,17 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from camera import CameraCv
 kivy.require('1.11.1')
 
-# class MyBoxLayout(BoxLayout):
-#     pass
-
-
-# class MyApp(App):
-
-#     def build(self):
-#         return MyBoxLayout()
-
-
-# if __name__ == '__main__':
-#     app = MyApp()
-#     app.run()
 
 class Screen1(Screen):
     pass
 
 class Screen2(Screen):
+    pass
+
+class Screen3(Screen):
     pass
 
 class ScreenManagement(ScreenManager):
