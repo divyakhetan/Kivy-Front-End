74a71d309e389dca6066a9dfba1e7ce24a74c24b
divyakhetan <divyakhetan1209@gmail.com>
2020-07-08 00:35:28 +0530

    all screens final

--- index.py
+++ index.py
@@ -1,26 +1,65 @@
 import kivy
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from camera import CameraCv
+from kivy.clock import Clock
+# from kivy.config import Config
+
 kivy.require('1.11.1')
+# Config.set('kivy', 'default_font', ['Roboto', 'data/fonts/Roboto-Regular.ttf', 'data/fonts/Roboto-Italic.ttf', 'data/fonts/Roboto-Bold.ttf', 'data/fonts/Roboto-BoldItalic.ttf'])
+# Config.write()
+# print('hello', Config.get('kivy', 'default_font'))
+
+class ScreenIntroa(Screen):
+    pass
+
+class ScreenIntrob(Screen):
+    pass
 
 
 class Screen1(Screen):
     pass
 
+class Screen1success(Screen):
+    pass
+
+class Screen1failure(Screen):
+    pass
+
 class Screen2(Screen):
     pass
 
-class Screen3(Screen):
+class Screen2success(Screen):
+    pass
+
+class Screen2failure(Screen):
+    pass
+
+class Screen3a(Screen):
+    pass
+
+class Screen3b(Screen):
+    pass
+
+class Screen3c(Screen):
+    pass
+
+class Screen3d(Screen):
+    pass
+
+class Screen4(Screen):
+    pass
+
+class Screen5(Screen):
     pass
 
 class ScreenManagement(ScreenManager):
     pass
 
 presentation = Builder.load_file("MyApp.kv")
+
 
 class MainApp(App):
     def build(self):
