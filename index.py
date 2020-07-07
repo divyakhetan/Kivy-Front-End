b7c55bdbc3019bdedc7801de79f7008dc1409e63
divyakhetan <divyakhetan1209@gmail.com>
2020-07-07 11:56:35 +0530

    final layout for one screen

--- index.py
+++ index.py
@@ -1,37 +1,39 @@
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 kivy.require('1.11.1')
 
-class MyBoxLayout(BoxLayout):
+# class MyBoxLayout(BoxLayout):
+#     pass
+
+
+# class MyApp(App):
+
+#     def build(self):
+#         return MyBoxLayout()
+
+
+# if __name__ == '__main__':
+#     app = MyApp()
+#     app.run()
+
+class Screen1(Screen):
     pass
 
+class Screen2(Screen):
+    pass
 
-class MyApp(App):
+class ScreenManagement(ScreenManager):
+    pass
 
+presentation = Builder.load_file("MyApp.kv")
+
+class MainApp(App):
     def build(self):
-        # mainLayout = BoxLayout(orientation="vertical")
-        
-        # hBoxLayout = BoxLayout(orientation="horizontal")
-        # btn1 = Button(text="click me 1")
-        # btn2 = Button(text="click me 2")
-        # hBoxLayout.add_widget(btn1)
-        # hBoxLayout.add_widget(btn2)
-
-        # vBoxLayout = BoxLayout(orientation="vertical")
-        # btn3 = Button(text="click me 3")
-        # btn4 = Button(text="click me 4")
-        # vBoxLayout.add_widget(btn3)
-        # vBoxLayout.add_widget(btn4)
-
-        # mainLayout.add_widget(hBoxLayout)
-        # mainLayout.add_widget(vBoxLayout)
-
-        return MyBoxLayout()
-
+        return presentation
 
 if __name__ == '__main__':
-    app = MyApp()
-    app.run()
+    MainApp().run()
